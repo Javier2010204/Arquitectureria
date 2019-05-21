e2334f2e79a8061d4f4778c2e6b8a04180d653ad
Javier2010204 <2010204javiergomez@gmail.com>
2019-05-21 12:15:39 -0600

    slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-$(window).scroll(function(){
-	console.log('hola')
-})
+let currentPosition = 0;
+const imageCounter = $("[data-name='image-counter']").attr("content")
+console.log(imageCounter)
+setInterval(()=>{
+
+	if (currentPosition < imageCounter) {
+		currentPosition++;
+	}else{
+		currentPosition = 0;
+	}
+
+	$("#gallery .inner").css({
+		left: "-"+currentPosition*100+"%"
+	})
+}, 3000)
